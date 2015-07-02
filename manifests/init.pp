49774acd760fd1de4dbcbf16aea5d7f0c15b4c3f
Josh Samuelson <js@puppetlabs.com>
2015-07-02 12:34:52 -0700

    Moving base and cleanup to bootstrap

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,23 +1,4 @@
 class student {
-  # ILT training-specific setup
-  File {
-    owner => 'root',
-    group => 'root',
-    mode  => '0644',
-  }
-
-  file { '/usr/bin/envpuppet':
-    source => 'puppet:///modules/bootstrap/envpuppet',
-    mode   => '0755',
-  }
-
-
-  # Add a few extra packages for convenience
-  package { [ 'patch', 'screen', 'telnet', 'tree', 'wget' ] :
-    ensure  => present,
-    require => Class['localrepo'],
-  }
-
 
   # configure user environment
   include userprefs::defaults
@@ -28,7 +9,4 @@ class student {
   # Add helper scripts
   include student::scripts
 
-
-  # Clean up extranous build stuff
-  include student::cleanup
 }
